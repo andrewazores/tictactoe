7175609c58b6d8071149fd2bea09777dcb2fada0
Andrew Azores <andrew.azores@gmail.com>
2014-02-03 21:42:09 -0500

    removed unnecessary parens and return statement

--- message.rb
+++ message.rb
@@ -15,18 +15,18 @@ class Message
     @message = ""
   end
 
-  def initialize(method, message)
+  def initialize method, message
     @method = method
     @message = message
   end
 
   def self.validate_message message
-    return false unless (["connect", "prompt", "move", "game", "error"].include? message.method)
+    return false unless ["connect", "prompt", "move", "game", "error"].include? message.method
 
     case message.method
       when "connect" then ["0", "1"].include? message.message
       when "prompt" then message.message.length == 9
-      when "move" then (message.message.to_i >= 0 and message.message.to_i <= 8)
+      when "move" then message.message.to_i >= 0 and message.message.to_i <= 8
       when "game" then ["win", "lose"].include? message.message
       when "error" then @errors.include? message.message.to_i
       else false
@@ -39,7 +39,7 @@ class Message
 
   def self.message_from_string string
     args = string.split
-    return Message.new(args[0], args[1])
+    Message.new args[0], args[1]
   end
 
   def to_string
