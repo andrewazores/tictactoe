c97ee376fb9d69128ddcf10db4dea9ae14ddbfbe
Andrew Azores <andrew.azores@gmail.com>
2014-02-03 21:16:20 -0500

    made server into a class, with small startup script to launch

--- server.rb
+++ server.rb
@@ -3,9 +3,151 @@
 require "socket"
 require "./message.rb"
 
-Signal.trap "TERM" do
-  p "Exiting"
-  @socket.close
+class Server
+
+  def initialize port
+    Signal.trap "TERM" do
+      p "Exiting"
+      @socket.close
+    end
+    @port = port
+    @player_turn = 0
+    @client_sockets = []
+    @board = [-1] * 9
+  end
+
+  def start
+    @socket = TCPServer.new @port
+
+    @socket.listen 5
+    puts "Server is: #{Socket.gethostname}"
+    puts "Waiting for clients"
+
+    (0...2).each do |i|
+      @client_sockets[i], client_info = @socket.accept
+      msg = Message.new "connect", i.to_s
+      puts "Player #{i.to_s} connected"
+      send_message msg, i
+    end
+    puts ""
+
+    while true do
+      prompt_msg = Message.new "prompt", board_to_string
+      send_message prompt_msg, @player_turn
+
+      begin
+        raw_msg = @client_sockets[@player_turn].recvfrom(16)[0].chomp
+      rescue
+        print "Connection to player #{@player_turn.to_s} lost"
+        break
+      end
+
+      if raw_msg.length <= 0
+        print "Connection to player #{@player_turn.to_s} lost"
+        send_message Message.new("error", 5), (@player_turn + 1) % 2
+        break
+      end
+
+      msg = Message.message_from_string raw_msg
+      unless Message.validate_message msg
+        puts "Invalid message received from player #{@player_turn.to_s}"
+        send_message Message.new("error", 0), @player_turn
+      end
+
+      valid, err_code = handle_message msg
+      unless valid
+        send_message Message.new("error", err_code), @player_turn
+        next
+      end
+
+      check_win
+      @player_turn = (@player_turn + 1) % 2 if Message.validate_message msg and valid
+    end
+  end
+
+  def shutdown
+    (0...2).each do |i|
+      @client_sockets[i].close
+    end
+    exit 0
+  end
+
+  def handle_message msg
+    mthd = msg.method.to_s
+    mess = msg.message.to_s
+
+    puts "\nReceived message from player #{@player_turn.to_s}:"
+    puts "\t#{mthd} #{mess}"
+
+    if mthd == "move"
+      if @board[mess.to_i] == -1
+        @board[mess.to_i] = @player_turn
+        return true, ""
+      else
+        return false, 5
+      end
+    else
+      puts "\tIllegal message method"
+      return false, 0
+    end
+  end
+
+  def send_message msg, player
+    puts "\nPlayer #{@player_turn.to_s} being sent message:"
+    puts "\t#{msg.to_string}"
+    @client_sockets[player].puts msg.to_string
+  end
+
+  def board_to_string
+    brd = ""
+    @board.each_index do |i|
+      brd += case @board[i]
+        when -1 then i.to_s
+        when 0 then 'X'
+        when 1 then 'O'
+      end
+    end
+    brd
+  end
+
+  def send_win
+    send_message Message.new("game", "win"), @player_turn
+    send_message Message.new("game", "lose"), (@player_turn + 1) % 2
+
+    exit 0
+  end
+
+  def send_tie
+    send_message Message.new("game", "lose"), 0
+    send_message Message.new("game", "lose"), 1
+
+    exit 0
+  end
+
+  def check_seq a, b, c
+    @board[a] != -1 and @board[a] == @board[b] and @board[b] == @board[c]
+  end
+
+  def check_win
+    (0...2).each do |i|
+      send_win if check_seq i, i + 3, i + 6
+    end
+
+    [0, 3, 6].each do |i|
+      send_win if check_seq i, i + 1, i + 2
+    end
+
+    send_win if check_seq 0, 4, 8
+    send_win if check_seq 2, 4, 6
+
+    board_filled = true
+    @board.each do |i|
+      board_filled = false if i == -1
+    end
+
+    send_tie if board_filled
+  end
+
 end
 
 if ARGV.length < 1
@@ -13,137 +155,7 @@ if ARGV.length < 1
     exit 1
 end
 
-@player_turn = 0
-@PORT = ARGV[0]
-@client_sockets = []
-@board = [-1] * 9
+port = ARGV[0]
 
-def handle_message msg
-  mthd = msg.method.to_s
-  mess = msg.message.to_s
-
-  puts "\nReceived message from player #{@player_turn.to_s}:"
-  puts "\t#{mthd} #{mess}"
-
-  if mthd == "move"
-    if @board[mess.to_i] == -1
-      @board[mess.to_i] = @player_turn
-      return true, ""
-    else
-      return false, 5
-    end
-  else
-    puts "\tIllegal message method"
-    return false, 0
-  end
-end
-
-def send_message(msg, player)
-  puts "\nPlayer #{@player_turn.to_s} being sent message:"
-  puts "\t#{msg.to_string}"
-  @client_sockets[player].puts msg.to_string
-end
-
-def board_to_string
-  brd = ""
-  @board.each_index do |i|
-    brd += case @board[i]
-      when -1 then i.to_s
-      when 0 then 'X'
-      when 1 then 'O'
-    end
-  end
-  brd
-end
-
-def send_win
-  send_message Message.new("game", "win"), @player_turn
-  send_message Message.new("game", "lose"), (@player_turn + 1) % 2
-
-  exit 0
-end
-
-def send_tie
-  send_message Message.new("game", "lose"), 0
-  send_message Message.new("game", "lose"), 1
-
-  exit 0
-end
-
-def check_seq(a, b, c)
-  @board[a] != -1 and @board[a] == @board[b] and @board[b] == @board[c]
-end
-
-def check_win
-  (0...2).each do |i|
-    send_win if check_seq(i, i + 3, i + 6)
-  end
-
-  [0, 3, 6].each do |i|
-    send_win if check_seq(i, i + 1, i + 2)
-  end
-
-  send_win if check_seq(0, 4, 8)
-  send_win if check_seq(2, 4, 6)
-
-  board_filled = true
-  @board.each do |i|
-    board_filled = false if i == -1
-  end
-
-  send_tie if board_filled
-end
-
-@socket = TCPServer.new @PORT
-
-@socket.listen 5
-puts "Server is: #{Socket.gethostname}"
-puts "Waiting for clients"
-
-(0...2).each do |i|
-  @client_sockets[i], client_info = @socket.accept
-  msg = Message.new "connect", i.to_s
-  puts "Player #{i.to_s} connected"
-  send_message msg, i
-end
-puts ""
-
-while true do
-  prompt_msg = Message.new "prompt", board_to_string
-  send_message prompt_msg, @player_turn
-
-  begin
-    raw_msg = @client_sockets[@player_turn].recvfrom(16)[0].chomp
-  rescue
-    print "Connection to player #{@player_turn.to_s} lost"
-    break
-  end
-
-  if raw_msg.length <= 0
-    print "Connection to player #{@player_turn.to_s} lost"
-    send_message Message.new("error", 5), (@player_turn + 1) % 2
-    break
-  end
-
-  msg = Message.message_from_string raw_msg
-  unless Message.validate_message msg
-    puts "Invalid message received from player #{@player_turn.to_s}"
-    send_message(Message.new("error", 0), @player_turn)
-  end
-
-  valid, err_code = handle_message msg
-  unless valid
-    send_message Message.new("error", err_code), @player_turn
-    next
-  end
-
-  check_win
-  @player_turn = (@player_turn + 1) % 2 if Message.validate_message msg and valid
-end
-
-(0...2).each do |i|
-  @client_sockets[i].close
-end
-
-exit 0
-
+server = Server.new port
+server.start
